49c17daab63d51946813edbed81bdb9b5b3fa318
Panos Anastasopoulos <panastasopoulos@helloworld.gr>
2023-09-13 00:40:44 +0300

    refactor: adds blade syntax

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,14 +2,14 @@
 <link rel="stylesheet" href="/app.css" />
 
 <body>
-    <?php foreach ($posts as $post) : ?>
-        <article>
-            <h1>
-                <a href="post/<?= $post->slug ?>">
-                    <?= $post->title ?>
-                </a>
-            </h1>
-            <?= $post->excerpt ?>
-        </article>
-    <?php endforeach; ?>
+    @foreach ($posts as $post)
+    <article>
+        <h1>
+            <a href="post/{{$post->slug}}">
+                {{$post->title }}
+            </a>
+        </h1>
+        {{$post->excerpt}}
+    </article>
+    @endforeach;
 </body>
