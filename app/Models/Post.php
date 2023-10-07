85beeac9fdf42d27076fa4708299151a4dd66d00
Panos Anastasopoulos <panastasopoulos@helloworld.gr>
2023-10-07 21:38:54 +0300

    feat: route model binding

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,5 +9,10 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'excerpt', 'date', 'body', 'slug'];
+    protected $fillable = ['title', 'excerpt', 'body'] ;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    } 
 }
